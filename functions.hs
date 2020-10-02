fdd99e70beccd6426bfc303b9d580501952c0d90
Vincent Tran <vincenttran197120@gmail.com>
2020-10-02 15:05:36 -0500

    Updated functions.hs

--- functions.hs
+++ functions.hs
@@ -5,5 +5,13 @@ testPrint :: String -> String
 testPrint "pattern" = "Matched" --Pattern matching (testPrint returns "Matched" if parameter is equal to "Pattern")
 testPrint currentString = currentString ++ "concat" -- If the parameter is anything, return the same value but plus 1
 
+sumAndAverage :: (Float, Float, Float) -> (Float, Float)  -- Multi-lined function
+sumAndAverage (a,b,c) = (average, sum) where 
+    sum = a + b + c -- sum
+    totalIndex = 3
+    average = a / totalIndex -- divide w/ float (/); divide w/ integer `div`
+    random = print("Completed")
+
+
 main = do 
-    print(testPrint "test")
+    print(sumAndAverage(1,2,3))
